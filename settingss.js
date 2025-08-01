f7f6a2637d3967107b68dfa09fb275f6b4f7a00a
yvan236 <ibra0008673@gmail.com>
2025-08-01 09:18:09 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,33 @@
-module.exports = {
-  SESSION_ID: " ",  // malvin~NuUASL4b#KBjtwUIHJmtMZLTbsIEwyyDDBiDfVuLVy8M3dJZy3_4~
-  OWNER_NUMBER: "+2250713697799", // PUT OWNER NUMBER HERE
-  PREFIX: "." // prefix (e.g., ., /, !, *)
-};
+name: Node.js CI
+
+on:
+  push:
+    branches:
+      - main
+  pull_request:
+    branches:
+      - main
+
+jobs:
+  build:
+
+    runs-on: ubuntu-latest
+
+    strategy:
+      matrix:
+        node-version: [20.x]
+
+    steps:
+    - name: Checkout repository
+      uses: actions/checkout@v3
+
+    - name: Set up Node.js
+      uses: actions/setup-node@v3
+      with:
+        node-version: ${{ matrix.node-version }}
+
+    - name: Install dependencies
+      run: npm install
+
+    - name: Start application
+      run: npm start
